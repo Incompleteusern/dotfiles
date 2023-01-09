0f8bb203b1351037cf0b958ff858411aabb3fe6a
Incompleteusern <58920010+Incompleteusern@users.noreply.github.com>
2023-01-09 09:15:41 -0700

    Make starting init.sh

--- init.sh
+++ init.sh
@@ -1,3 +1,27 @@
 #!/bin/bash
 
 BASEDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+# Add wheel to sudoers
+sed -i "s/#%wheel ALL=(ALL) ALL/%wheel ALL=(ALL) ALL" /etc/sudoers
+
+# Add colors to /etc/pacman.conf
+sed -i "s/#Color/Color" /etc/pacman.conf
+
+# yay
+pacman -S git base-devel
+git clone https://aur.archlinux.org/yay.git
+cd yay
+makepkg -si
+cd ..
+rm -rf yay
+
+# network manager and add local host to /etc/hosts
+yay -S networkmanager
+echo -e "127.0.0.1        localhost\n::1              localhost" >> /etc/hosts
+
+# nano
+yay -S nano nano-syntax-highlighting
+
+# vim
+ln -s ${BASEDIR}/config ~/.config
