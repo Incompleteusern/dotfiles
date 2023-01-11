e569be5faa605578997e670e5b22a067d561d7b5
Incompleteusern <58920010+Incompleteusern@users.noreply.github.com>
2023-01-10 22:05:02 -0700

    Update init.sh

--- init.sh
+++ init.sh
@@ -16,7 +16,7 @@ cp -a ${BASEDIR}/config/ ~/.config/
 # DESKTOP
 
 # hyprland
-yay -S --noconfirm hyprland-git dunst waybar-hyprland-git
+yay -S --noconfirm hyprland-git dunst waybar-hyprland-git swww
 
 # pipewire
 yay -S --noconfirm pipewire pipewire-jack wireplumber
