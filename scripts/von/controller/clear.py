eab46c16822fbe5206574c4ecc0cd0ee246ef0b3
royce <royce.yao.ry@Gmail.com>
2023-02-15 11:35:46 -0700

    unbundle von

--- scripts/von/controller/clear.py
+++
@@ -1,7 +0,0 @@
-from .. import model, view
-
-parser = view.Parser(prog="clear", description="Clears the Cache.")
-
-
-def main(self: object, argv: list[str]):
-    model.clearCache()
--- scripts/von/controller/index.py
+++
@@ -1,9 +0,0 @@
-from .. import model, view
-
-parser = view.Parser(
-    prog="index", description="Rebuilds the problem index. No arguments."
-)
-
-
-def main(self: object, argv: list[str]):
-    model.rebuildIndex()
